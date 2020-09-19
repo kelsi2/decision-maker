4caec478da6409aa0529a2d80f335ec249343c4d
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-09-19 19:16:37 +0000

    add user data

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,405 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+create table users
+(
+  id INT,
+  email VARCHAR(50)
+);
+insert into users
+  (id, email)
+values
+  (1, 'myeiles0@shareasale.com');
+insert into users
+  (id, email)
+values
+  (2, 'btunnick1@hao123.com');
+insert into users
+  (id, email)
+values
+  (3, 'fjude2@china.com.cn');
+insert into users
+  (id, email)
+values
+  (4, 'tdunbleton3@ning.com');
+insert into users
+  (id, email)
+values
+  (5, 'cashburne4@usda.gov');
+insert into users
+  (id, email)
+values
+  (6, 'rleser5@eventbrite.com');
+insert into users
+  (id, email)
+values
+  (7, 'irowter6@opensource.org');
+insert into users
+  (id, email)
+values
+  (8, 'jmyers7@hubpages.com');
+insert into users
+  (id, email)
+values
+  (9, 'bstripling8@privacy.gov.au');
+insert into users
+  (id, email)
+values
+  (10, 'haubry9@harvard.edu');
+insert into users
+  (id, email)
+values
+  (11, 'aiacovidesa@unicef.org');
+insert into users
+  (id, email)
+values
+  (12, 'cdettmarb@google.it');
+insert into users
+  (id, email)
+values
+  (13, 'emallanc@ning.com');
+insert into users
+  (id, email)
+values
+  (14, 'foldingd@alexa.com');
+insert into users
+  (id, email)
+values
+  (15, 'jbroadleye@google.es');
+insert into users
+  (id, email)
+values
+  (16, 'lwatersonf@fotki.com');
+insert into users
+  (id, email)
+values
+  (17, 'psmeadg@ucsd.edu');
+insert into users
+  (id, email)
+values
+  (18, 'fbloggetth@alexa.com');
+insert into users
+  (id, email)
+values
+  (19, 'rbiggini@ask.com');
+insert into users
+  (id, email)
+values
+  (20, 'nlambertsonj@rakuten.co.jp');
+insert into users
+  (id, email)
+values
+  (21, 'cberkeleyk@paginegialle.it');
+insert into users
+  (id, email)
+values
+  (22, 'kharrall@google.pl');
+insert into users
+  (id, email)
+values
+  (23, 'anicolsonm@wikispaces.com');
+insert into users
+  (id, email)
+values
+  (24, 'mdoweyn@twitpic.com');
+insert into users
+  (id, email)
+values
+  (25, 'rbortolozzio@telegraph.co.uk');
+insert into users
+  (id, email)
+values
+  (26, 'jmclarenp@reuters.com');
+insert into users
+  (id, email)
+values
+  (27, 'esandayq@elegantthemes.com');
+insert into users
+  (id, email)
+values
+  (28, 'jseyr@apache.org');
+insert into users
+  (id, email)
+values
+  (29, 'cdunlops@wordpress.org');
+insert into users
+  (id, email)
+values
+  (30, 'hdurtnelt@ning.com');
+insert into users
+  (id, email)
+values
+  (31, 'aharmesu@un.org');
+insert into users
+  (id, email)
+values
+  (32, 'mrenshallv@accuweather.com');
+insert into users
+  (id, email)
+values
+  (33, 'tbatramw@webnode.com');
+insert into users
+  (id, email)
+values
+  (34, 'mhoultonx@friendfeed.com');
+insert into users
+  (id, email)
+values
+  (35, 'egametty@mit.edu');
+insert into users
+  (id, email)
+values
+  (36, 'rmurdenz@nih.gov');
+insert into users
+  (id, email)
+values
+  (37, 'cmeece10@creativecommons.org');
+insert into users
+  (id, email)
+values
+  (38, 'dnarracott11@paginegialle.it');
+insert into users
+  (id, email)
+values
+  (39, 'jaymer12@house.gov');
+insert into users
+  (id, email)
+values
+  (40, 'yegiloff13@dion.ne.jp');
+insert into users
+  (id, email)
+values
+  (41, 'flaurence14@odnoklassniki.ru');
+insert into users
+  (id, email)
+values
+  (42, 'rmcphaden15@is.gd');
+insert into users
+  (id, email)
+values
+  (43, 'cgriffey16@360.cn');
+insert into users
+  (id, email)
+values
+  (44, 'rtonkin17@prnewswire.com');
+insert into users
+  (id, email)
+values
+  (45, 'mwarden18@artisteer.com');
+insert into users
+  (id, email)
+values
+  (46, 'nkneath19@themeforest.net');
+insert into users
+  (id, email)
+values
+  (47, 'jstirley1a@va.gov');
+insert into users
+  (id, email)
+values
+  (48, 'krosellini1b@ed.gov');
+insert into users
+  (id, email)
+values
+  (49, 'ebourner1c@princeton.edu');
+insert into users
+  (id, email)
+values
+  (50, 'ccone1d@1688.com');
+insert into users
+  (id, email)
+values
+  (51, 'dkeirle1e@accuweather.com');
+insert into users
+  (id, email)
+values
+  (52, 'btarbox1f@theglobeandmail.com');
+insert into users
+  (id, email)
+values
+  (53, 'vespino1g@netlog.com');
+insert into users
+  (id, email)
+values
+  (54, 'calf1h@timesonline.co.uk');
+insert into users
+  (id, email)
+values
+  (55, 'fmouget1i@hatena.ne.jp');
+insert into users
+  (id, email)
+values
+  (56, 'nspittle1j@joomla.org');
+insert into users
+  (id, email)
+values
+  (57, 'anapper1k@latimes.com');
+insert into users
+  (id, email)
+values
+  (58, 'vmapstone1l@google.com.hk');
+insert into users
+  (id, email)
+values
+  (59, 'dfillan1m@cmu.edu');
+insert into users
+  (id, email)
+values
+  (60, 'cjori1n@google.co.uk');
+insert into users
+  (id, email)
+values
+  (61, 'mfrondt1o@nyu.edu');
+insert into users
+  (id, email)
+values
+  (62, 'egwillyam1p@chronoengine.com');
+insert into users
+  (id, email)
+values
+  (63, 'aeaslea1q@va.gov');
+insert into users
+  (id, email)
+values
+  (64, 'vjobe1r@webs.com');
+insert into users
+  (id, email)
+values
+  (65, 'aelleton1s@usda.gov');
+insert into users
+  (id, email)
+values
+  (66, 'gdukes1t@diigo.com');
+insert into users
+  (id, email)
+values
+  (67, 'acoffin1u@typepad.com');
+insert into users
+  (id, email)
+values
+  (68, 'cgeram1v@dyndns.org');
+insert into users
+  (id, email)
+values
+  (69, 'fburtonshaw1w@youku.com');
+insert into users
+  (id, email)
+values
+  (70, 'lharnes1x@dyndns.org');
+insert into users
+  (id, email)
+values
+  (71, 'epedrozzi1y@biblegateway.com');
+insert into users
+  (id, email)
+values
+  (72, 'emuress1z@seattletimes.com');
+insert into users
+  (id, email)
+values
+  (73, 'jsexton20@tripod.com');
+insert into users
+  (id, email)
+values
+  (74, 'mcanadas21@zdnet.com');
+insert into users
+  (id, email)
+values
+  (75, 'jconfait22@fastcompany.com');
+insert into users
+  (id, email)
+values
+  (76, 'dchevin23@moonfruit.com');
+insert into users
+  (id, email)
+values
+  (77, 'gpinkerton24@foxnews.com');
+insert into users
+  (id, email)
+values
+  (78, 'jlocket25@barnesandnoble.com');
+insert into users
+  (id, email)
+values
+  (79, 'ebriar26@woothemes.com');
+insert into users
+  (id, email)
+values
+  (80, 'kplaskitt27@nasa.gov');
+insert into users
+  (id, email)
+values
+  (81, 'mfinnemore28@boston.com');
+insert into users
+  (id, email)
+values
+  (82, 'mshellibeer29@yandex.ru');
+insert into users
+  (id, email)
+values
+  (83, 'pboucher2a@wired.com');
+insert into users
+  (id, email)
+values
+  (84, 'jmishaw2b@microsoft.com');
+insert into users
+  (id, email)
+values
+  (85, 'nstorch2c@mozilla.org');
+insert into users
+  (id, email)
+values
+  (86, 'kbasketfield2d@accuweather.com');
+insert into users
+  (id, email)
+values
+  (87, 'mfeake2e@behance.net');
+insert into users
+  (id, email)
+values
+  (88, 'melvidge2f@webeden.co.uk');
+insert into users
+  (id, email)
+values
+  (89, 'jandreou2g@squarespace.com');
+insert into users
+  (id, email)
+values
+  (90, 'lzamudio2h@dropbox.com');
+insert into users
+  (id, email)
+values
+  (91, 'ewoolway2i@jalbum.net');
+insert into users
+  (id, email)
+values
+  (92, 'mdanhel2j@blogtalkradio.com');
+insert into users
+  (id, email)
+values
+  (93, 'sandrich2k@addtoany.com');
+insert into users
+  (id, email)
+values
+  (94, 'msprigin2l@google.co.uk');
+insert into users
+  (id, email)
+values
+  (95, 'ateodori2m@multiply.com');
+insert into users
+  (id, email)
+values
+  (96, 'kdarbon2n@tuttocitta.it');
+insert into users
+  (id, email)
+values
+  (97, 'gsimenon2o@apache.org');
+insert into users
+  (id, email)
+values
+  (98, 'vregus2p@chicagotribune.com');
+insert into users
+  (id, email)
+values
+  (99, 'twheatley2q@ustream.tv');
+insert into users
+  (id, email)
+values
+  (100, 'ahoogendorp2r@rakuten.co.jp');
