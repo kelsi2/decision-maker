83ed32a48913a958e98b3dd2d275fd4e70e396b8
Raphael Cordero <raffycordero2001@yahoo.ca>
2020-09-19 17:08:14 +0000

    added button for more inputs

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,26 +1,13 @@
 jQuery(document).ready(function() {
 
-  var maxField = 10; //Input fields increment limitation
-  var addButton = $('.add_button'); //Add button selector
-  var wrapper = $('.field_wrapper'); //Input field wrapper
-  var inputHTML = '<div><input type="text" name="field_name[]" value=""/><a href="javascript:void(0);" class="remove_button"></a></div>'; //New input field html
-  var x = 1; //Initial field counter is 1
+  $("#addOptionBtn").on('click', function (event) {
 
-  //Once add button is clicked
-  $(addButton).click(function(){
-    console.log(test);
-      //Check maximum number of input fields
-      if(x < maxField){
-          x++; //Increment field counter
-          $(wrapper).append(inputHTML); //Add field html
-      }
-  });
+    event.preventDefault();
+    $('.field_wrapper').append($('<input><br>')
+    .attr('type', 'text')
+    .addClass("options")
+    .attr('placeholder', `Third Option`))
 
-  //Once remove button is clicked
-  $(wrapper).on('click', '.remove_button', function(e){
-      e.preventDefault();
-      $(this).parent('div').remove(); //Remove field html
-      x--; //Decrement field counter
-  });
+  })
 });
 
