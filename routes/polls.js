aca63ba81e63e72705559927bf138c8193592569
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-09-20 20:07:30 +0000

    update to all styles

--- routes/polls.js
+++ routes/polls.js
@@ -1,20 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-//Create new poll
-// router.post('/', (req, res) => {
-//generate link
-// const adminID = generateRandomString();
-// const voterID = generateRandomString();
-// let options = [];
-
-// const newPoll = {
-//   email: req.body.email,
-//   title: req.body.title,
-//   adminURL: adminID,
-//   voterURL: voterID,
-//   voteCount: 0
-// };
 module.exports = function(database) {
   //create new poll
   router.get("/", (req, res) => {
