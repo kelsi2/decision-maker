c201acc0e2ff7b9230fca6cf7d3c5294c0ca339d
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-09-22 19:57:36 +0000

    chart formatting

--- routes/admin.js
+++ routes/admin.js
@@ -1,28 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-module.exports = function(database) {
+module.exports = function (database) {
   router.get("/:pollId", (req, res) => {
     const pollId = req.params.pollId;
-    database.getPolls(pollId)
-      .then((result) => {
-        const promiseArray = result.map(option => {
-          return database.getTotalRank(option.id, pollId);
-        });
-        return Promise.all(promiseArray)
-          .then((object) => {
-            console.log('object', object);
-            console.log('result', result);
-
-            const templateVars = {
-              data: object,
-              title: result[0].title,
-              description: result[0].description
-            };
-            res.render("poll_admin", templateVars);
-          })
-          .catch((err) => console.log(err));
+    database.getPolls(pollId).then((result) => {
+      const promiseArray = result.map((option) => {
+        return database.getTotalRank(option.id, pollId);
       });
+      return Promise.all(promiseArray)
+
+        .then((object) => {
+          const templateVars = {
+            data: object,
+            title: result[0].title,
+            description: result[0].description,
+          };
+          res.render("poll_admin", templateVars);
+        })
+        .catch((err) => console.log(err));
+    });
   });
 
   return router;
