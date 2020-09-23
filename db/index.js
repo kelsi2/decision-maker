f2d1af44bd54a3d112ebcffe32d2aee3296b61d5
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-09-23 23:12:59 +0000

    format email check page

--- db/index.js
+++ db/index.js
@@ -1,14 +1,12 @@
-const {Pool} = require('pg');
+const { Client } = require("pg");
 
-const dbParams = require('../lib/db.js');
-const db = new Pool(dbParams);
+const dbParams = require("../lib/db.js");
+const db = new Client(dbParams);
 db.connect(() => {
   console.log("Connected to database");
 });
 module.exports = {
   query: (text, params, callback) => {
-    return db.query(text, params, callback)
+    return db.query(text, params, callback);
   },
-}
-
-
+};
