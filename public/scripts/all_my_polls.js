d75f5bd9c7755e0970e560d232f47a8e71bf4cde
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-09-24 16:28:26 +0000

    style poll_admin email check

--- public/scripts/all_my_polls.js
+++ public/scripts/all_my_polls.js
@@ -1,28 +1,28 @@
-jQuery(document).ready(function() {
-  $('#allPolls').on('submit', (e)=> {
+jQuery(document).ready(function () {
+  $("#allPolls").on("submit", (e) => {
     //prevent the browser from redirecting to json page
     e.preventDefault();
-    let email = $('#allPollEmail').val();
+    let email = $("#allPollEmail").val();
     fetchId(email);
-  })
+  });
 
   const fetchId = (email) => {
     $.ajax({
       url: `/admin/hello?email=${email}`,
-      method: 'GET',
-      dataType: 'json',
+      method: "GET",
+      dataType: "json",
       success: (links) => {
         let count = 1;
         for (let link of links) {
-          $('.poll-container').append(`<br><a class='poll-links' href=/admin/${link.id}>poll ${count}</a><br>`)
-          count ++;
+          $(".poll-container").append(
+            `<br><a class='poll-links' href=/admin/${link.id}>Poll ${count}</a><br>`
+          );
+          count++;
         }
-
       },
       error: (error) => {
         console.error(error);
-      }
+      },
     });
-  }
-
-})
+  };
+});
